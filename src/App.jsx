ecda98f664eb50aa387094c3b271a2bcb6a01bd6
Mohammad Chohan <mohammadchohan6f@gmail.com>
2023-11-14 00:38:46 +1100

    replace/delete old implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,73 @@
-import { useState } from "react";
-import generateArray from "./generateArray.js";
-import SortButton from "./SortButton.jsx";
-import useBubbleSort from "./useBubbleSort.js";
-import useMergeSort from "./useMergeSort.js";
-import useQuickSort from "./useQuickSort.js";
-import useSelectionSort from "./useSelectionSort.js";
+import React, { useState, useRef } from "react";
+import {
+  bubbleSortGenerator,
+  insertionSortGenerator,
+  quickSortGenerator,
+  selectionSortGenerator,
+  mergeSortGenerator,
+  hoareQuickSortGenerator,
+} from "./sortingAlgorithms";
+import generateArray from "./generateArray";
+import SortButton from "./SortButton";
+
+const DEFAULT_ARRAY_SIZE = 6;
+const sorters = {
+  "Bubble Sort": {
+    generator: bubbleSortGenerator,
+    colorFunction: bubbleSortColors,
+  },
+  "Quick Sort (Hoare)": {
+    generator: hoareQuickSortGenerator,
+    colorFunction: hoareQuickSortColors,
+  },
+  "Insertion Sort": {
+    generator: insertionSortGenerator,
+    colorFunction: insertionSortColors,
+  },
+  "Quick Sort (Lomuto)": {
+    generator: quickSortGenerator,
+    colorFunction: quickSortColors,
+  },
+  "Selection Sort": {
+    generator: selectionSortGenerator,
+    colorFunction: selectionSortColors,
+  },
+  "Merge Sort": {
+    generator: mergeSortGenerator,
+    colorFunction: mergeSortColors,
+  },
+};
 
 export default function App() {
-  const [list, setList] = useState(generateArray(6));
-  const [inProgress, setInProgress] = useState(false);
-  const [checked, setChecked] = useState("");
-  const [completed, setCompleted] = useState(false);
-  const [sorted, setSorted] = useState(false);
-
-  const { mergeSort, mergeState } = useMergeSort(
-    list,
-    setList,
-    completed,
-    setCompleted,
-    inProgress,
-    setInProgress,
-    setSorted,
+  const [list, setList] = useState(() => generateArray(DEFAULT_ARRAY_SIZE));
+  const [isSorting, setIsSorting] = useState(false);
+  const [selectedSorter, setSelectedSorter] = useState("Bubble Sort");
+  const sortGenerator = useRef(null);
+  const [colors, setColors] = useState(() =>
+    Array(list.length).fill("bg-slate-800"),
   );
 
-  const { bubbleSort, bubbleState } = useBubbleSort(
-    list,
-    setList,
-    completed,
-    setCompleted,
-    inProgress,
-    setInProgress,
-    setSorted,
-  );
+  function stepSort() {
+    const { value, done } = sortGenerator.current.next();
+    if (!done) {
+      setList(value.array);
+      setColors(sorters[selectedSorter].colorFunction(value));
+      setTimeout(stepSort, Math.floor(2400 / list.length));
+    } else {
+      setIsSorting(false);
+      flashGreen();
+    }
+  }
 
-  const { quickSort, quickState } = useQuickSort(
-    list,
-    setList,
-    completed,
-    setCompleted,
-    inProgress,
-    setInProgress,
-    setSorted,
-  );
+  function flashGreen() {
+    setColors(Array(list.length).fill("bg-green-300"));
+    setTimeout(() => setColors(Array(list.length).fill("bg-purple-300")), 500);
+  }
 
-  const { selectionSort, selectionState } = useSelectionSort(
-    list,
-    setList,
-    completed,
-    setCompleted,
-    inProgress,
-    setInProgress,
-    setSorted,
-  );
-
-  const sortingAlgorithms = {
-    "Merge Sort": mergeSort,
-    "Bubble Sort": bubbleSort,
-    "Selection Sort": selectionSort,
-    "Quick Sort": quickSort,
-  };
-
-  if (inProgress) {
-    const delay = completed ? 500 : Math.max(0, Math.floor(2400 / list.length));
-    setTimeout(sortingAlgorithms[checked], delay);
+  function startSorting() {
+    sortGenerator.current = sorters[selectedSorter].generator(list);
+    setIsSorting(true);
+    stepSort();
   }
 
   return (
@@ -73,108 +78,32 @@ export default function App() {
           min="2"
           max="200"
           value={list.length}
-          disabled={inProgress}
+          disabled={isSorting}
           onChange={(e) => {
             const size = +e.target.value;
-            setList(generateArray(size));
-            setSorted(false);
+            const newList = generateArray(size);
+            setList(newList);
+            setColors(Array(newList.length).fill("bg-slate-800"));
           }}
         />
-        {Object.keys(sortingAlgorithms).map((algo) => (
-          <label className="flex items-center gap-0.5" key={algo}>
+        {Object.keys(sorters).map((sorterKey) => (
+          <label className="flex items-center gap-0.5" key={sorterKey}>
             <input
               type="radio"
-              name="algorithm"
-              value={algo}
-              disabled={inProgress}
-              checked={algo === checked}
-              onChange={(e) => {
-                setChecked(e.target.value);
-              }}
+              value={sorterKey}
+              checked={selectedSorter === sorterKey}
+              onChange={(e) => setSelectedSorter(e.target.value)}
+              disabled={isSorting}
             />
-            <span>{algo}</span>
+            {sorterKey}
           </label>
         ))}
-        <SortButton
-          disabled={checked === "" || inProgress}
-          onClick={sortingAlgorithms[checked]}
-        />
-      </div>
 
+        <SortButton disabled={isSorting} onClick={startSorting} />
+      </div>
       <div className="flex flex-grow justify-center gap-[1px]">
         {list.map((num, i) => {
-          let bg = "bg-slate-800";
-
-          if (inProgress) {
-            if (checked === "Bubble Sort") {
-              const { index, swappers, maxIndex } = bubbleState;
-              if ([index, index + 1].includes(i)) {
-                bg = "bg-green-300";
-              }
-              if (swappers.indices.includes(i)) {
-                bg = "bg-red-300";
-              }
-              if (i > maxIndex) {
-                bg = "bg-purple-300";
-              }
-            } else if (checked === "Merge Sort") {
-              const { swappers, a, b, width } = mergeState;
-              if (i === a || i === b) {
-                bg = "bg-green-300";
-              }
-              if (swappers.indices.includes(i)) {
-                bg = "bg-red-300";
-              }
-              if (width * 2 >= list.length && i < a) {
-                bg = "bg-purple-300";
-              }
-            } else if (checked === "Selection Sort") {
-              const { a, b, bMin, swappers } = selectionState;
-              if (i === a) {
-                bg = "bg-blue-300";
-              }
-              if (i === bMin) {
-                bg = "bg-yellow-300";
-              }
-              if (i === b) {
-                bg = "bg-green-300";
-              }
-              if (swappers.indices.includes(i)) {
-                bg = "bg-red-300";
-              }
-              if (i < a) {
-                bg = "bg-purple-300";
-              }
-            } else {
-              const { stack, a, b, pivots, swappers } = quickState;
-              if (stack.length) {
-                const { y } = stack[stack.length - 1];
-                if (i === y) {
-                  bg = "bg-yellow-300";
-                }
-              }
-              if (pivots.includes(i)) {
-                bg = "bg-purple-300";
-              }
-              if (i === a) {
-                bg = "bg-green-300";
-              }
-              if (i === b) {
-                bg = "bg-blue-300";
-              }
-              if (swappers.indices.includes(i)) {
-                bg = "bg-red-300";
-              }
-            }
-          }
-
-          if (sorted) {
-            bg = "bg-purple-300";
-          }
-
-          if (completed) {
-            bg = "bg-green-300";
-          }
+          let bg = colors[i];
 
           return (
             <div
@@ -187,4 +116,112 @@ export default function App() {
       </div>
     </div>
   );
+}
+
+function bubbleSortColors({ array, i, j, state }) {
+  const colors = Array(array.length).fill("bg-slate-800");
+  if (state === "pre-compare") {
+    colors[j] = "bg-green-300";
+    colors[j + 1] = "bg-green-300";
+  } else if (state === "pre-swap" || state === "post-swap") {
+    colors[j] = "bg-red-300";
+    colors[j + 1] = "bg-red-300";
+  }
+
+  // The last i elements are already sorted
+  for (let k = colors.length - i; k < colors.length; k++) {
+    colors[k] = "bg-purple-300";
+  }
+
+  return colors;
+}
+
+function quickSortColors({ array, end, i, partitionIndex, pivots, state }) {
+  const colors = Array(array.length).fill("bg-slate-800");
+  pivots.forEach((i) => (colors[i] = "bg-purple-300"));
+
+  if (state === "pre-compare") {
+    colors[i] = "bg-blue-300";
+    colors[end] = "bg-yellow-300";
+    colors[partitionIndex] = "bg-green-300";
+  } else if (state === "pre-swap 1" || state === "post-swap 1") {
+    colors[i] = "bg-red-300";
+    colors[end] = "bg-yellow-300";
+    colors[partitionIndex] = "bg-red-300";
+  } else if (state === "pre-swap 2" || state === "post-swap 2") {
+    colors[partitionIndex] = "bg-orange-300";
+    colors[end] = "bg-orange-300";
+  }
+
+  return colors;
+}
+
+function hoareQuickSortColors({ array, left, right, pivotIndex, state }) {
+  const colors = Array(array.length).fill("bg-slate-800");
+  colors[pivotIndex] = "bg-yellow-300";
+  if (state === "pre-compare") {
+    colors[left] = "bg-green-300";
+    colors[right] = "bg-blue-300";
+  } else if (state === "pre-swap" || state === "post-swap") {
+    colors[left] = "bg-red-300";
+    colors[right] = "bg-red-300";
+  }
+  return colors;
+}
+
+function insertionSortColors({ array, i, j, state }) {
+  const colors = Array(array.length).fill("bg-slate-800");
+  colors[i] = "bg-yellow-300";
+  if (state === "pre-swap" || state === "post-swap") {
+    colors[j] = "bg-red-300";
+    colors[j + 1] = "bg-red-300";
+  } else if (state === "pre-compare") {
+    colors[j] = "bg-green-300";
+    colors[j + 1] = "bg-green-300";
+  }
+  return colors;
+}
+
+function selectionSortColors({ array, start, min, i, state }) {
+  const colors = Array(array.length).fill("bg-slate-800");
+  colors[min] = "bg-yellow-300";
+  colors[start] = "bg-green-300";
+  if (state === "pre-compare") {
+    colors[i] = "bg-blue-300";
+  } else if (state === "pre-swap" || state === "post-swap") {
+    colors[min] = "bg-red-300";
+    colors[start] = "bg-red-300";
+  }
+
+  for (let a = 0; a < start; a++) {
+    colors[a] = "bg-purple-300";
+  }
+
+  return colors;
+}
+
+function mergeSortColors({ array, left, right, start, end, state }) {
+  const colors = Array(array.length).fill("bg-slate-800");
+  if (start === 0 && end === array.length - 1) {
+    for (let i = 0; i < left; i++) {
+      colors[i] = "bg-purple-300";
+    }
+  }
+  if (state === "pre-compare") {
+    colors[left] = "bg-green-300";
+    colors[right] = "bg-green-300";
+  } else if (state === "pre-move-left") {
+    colors[left] = "bg-red-300";
+    colors[right] = "bg-green-300";
+  } else if (state === "post-move-left") {
+    colors[left - 1] = "bg-red-300";
+    colors[right] = "bg-green-300";
+  } else if (state === "pre-move-right") {
+    colors[right] = "bg-red-300";
+    colors[left] = "bg-green-300";
+  } else if (state === "post-move-right") {
+    colors[left - 1] = "bg-red-300";
+    colors[left] = "bg-green-300";
+  }
+  return colors;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App2 from "./App2.jsx";
+import App from "./App";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App2 />
+    <App />
   </StrictMode>,
 );
