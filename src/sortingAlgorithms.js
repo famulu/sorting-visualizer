d36de29f3dcaeaac37eb020a41bc4d4c9684d8b4
Mohammad Chohan <mohammadchohan6f@gmail.com>
2023-11-13 19:14:28 +1100

    implemented sorting algorithms as generators instead of custom hooks

--- src/sortingAlgorithms.js
+++ src/sortingAlgorithms.js
@@ -1,137 +1,239 @@
-function quickSort(arr) {
-  const temp = [...arr];
-  // Stack for storing start and end index
-  const stack = [{ x: 0, y: temp.length - 1 }];
+export function* quickSortGenerator(initialArray) {
+  const array = [...initialArray];
 
-  //Iterate the stack
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const stack = [{ start: 0, end: array.length - 1 }];
+  const pivots = new Set();
+
   while (stack.length) {
-    // Get the start and end from the stack
-    const { x, y } = stack[stack.length - 1];
+    const { start, end } = stack.pop();
 
-    // Pick the last element as pivot
-    const pivot = temp[y];
-    let a = x;
+    const pivot = array[end];
+    let partitionIndex = start;
 
-    for (let b = x; b < y; b++) {
-      if (temp[b] <= pivot) {
-        [temp[a], temp[b]] = [temp[b], temp[a]];
-        a++;
+    for (let i = start; i < end; i++) {
+      yield {array, end, partitionIndex, i, pivots, state: "pre-compare"};
+      if (array[i] <= pivot) {
+        yield {array, end, partitionIndex, i, pivots, state: "pre-swap 1"};
+        [array[i], array[partitionIndex]] = [array[partitionIndex], array[i]];
+        yield {array,end,partitionIndex, i,pivots,state: "post-swap 1",};
+        partitionIndex++;
       }
     }
 
-    [temp[a], temp[y]] = [temp[y], temp[a]];
+    yield {array,end,partitionIndex,pivots,state: "pre-swap 2",};
+    // Place pivot in the correct position
+    [array[partitionIndex], array[end]] = [array[end], array[partitionIndex]];
+    pivots.add(partitionIndex);
+    yield {array,end,partitionIndex,pivots,state: "post-swap 2",};
 
-    stack.pop();
-
-    // Push sub array with fewer elements than pivot into the stack
-    if (a - 1 > x) {
-      stack.push({ x: x, y: a - 1 });
+    // Push subarrays into the stack
+    // Subarrays must have at least 2 elements
+    if (partitionIndex + 1 < end) {
+      stack.push({ start: partitionIndex + 1, end: end });
+    } else {
+      pivots.add(end);
     }
 
-    // Push sub array with greater elements than pivot into the stack
-    if (a + 1 < y) {
-      stack.push({ x: a + 1, y: y });
+    if (partitionIndex - 1 > start) {
+      stack.push({ start: start, end: partitionIndex - 1 });
+    } else {
+      pivots.add(start);
     }
   }
 
-  return temp;
+  return array;
 }
 
-function hoareQuickSort(arr) {
-  const temp = [...arr];
-  // Stack for storing start and end index
-  const stack = [{ x: 0, y: temp.length - 1 }];
+export function* hoareQuickSortGenerator(initialArray) {
+  const array = [...initialArray];
 
-  //Iterate the stack
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const stack = [{ start: 0, end: array.length - 1 }];
+
   while (stack.length) {
-    // Get the start and end from the stack
-    const { x, y } = stack[stack.length - 1];
-
-    // Pick the last element as pivot
-    const pivotIndex = Math.floor(x + (y - x) / 2);
-    const pivot = temp[pivotIndex];
-    let a = x - 1;
-    let b = y + 1;
+    const { start, end } = stack.pop();
+    let pivotIndex = Math.floor(start + (end - start) / 2);
+    const pivot = array[pivotIndex];
+    let left = start - 1;
+    let right = end + 1;
 
     while (true) {
       do {
-        a++;
-      } while (temp[a] < pivot);
+        left++;
+        yield { array, left, right, pivotIndex, state: "pre-compare" };
+      } while (array[left] < pivot);
       do {
-        b--;
-      } while (temp[b] > pivot);
+        right--;
+        yield { array, left, right, pivotIndex, state: "pre-compare" };
+      } while (array[right] > pivot);
 
-      if (a >= b) {
+      if (left >= right) {
         break;
       }
-      [temp[a], temp[b]] = [temp[b], temp[a]];
+
+      yield { array, left, right, pivotIndex, state: "pre-swap" };
+      [array[left], array[right]] = [array[right], array[left]];
+      if (left === pivotIndex) {
+        pivotIndex = right;
+      } else if (right === pivotIndex) {
+        pivotIndex = left;
+      }
+      yield { array, left, right, pivotIndex, state: "post-swap" };
     }
 
-    stack.pop();
-
-    // Push sub array with greater elements than pivot into the stack
-    if (b + 1 >= 0 && b + 1 < y) {
-      stack.push({ x: b + 1, y: y });
+    if (right + 1 < end) {
+      stack.push({ start: right + 1, end: end });
     }
-    // Push sub array with fewer elements than pivot into the stack
-    if (x >= 0 && x < b) {
-      stack.push({ x: x, y: b });
+    if (start < right) {
+      stack.push({ start: start, end: right });
     }
   }
 
-  return temp;
+  return array;
 }
 
-function selectionSort(array) {
-  const temp = [...array];
-  for (let i = 0; i < temp.length - 1; i++) {
-    let jMin = i;
-    for (let j = i + 1; j < temp.length; j++) {
-      if (temp[j] < temp[jMin]) {
-        jMin = j;
+
+export function* selectionSortGenerator(initialArray) {
+  const array = [...initialArray];
+
+  for (let start = 0; start < array.length - 1; start++) {
+    let min = start;
+    for (let i = start + 1; i < array.length; i++) {
+      yield { array, start, min, i, state: "pre-compare" };
+      if (array[i] < array[min]) {
+        min = i;
       }
     }
 
-    if (jMin !== i) {
-      [temp[jMin], temp[i]] = [temp[i], temp[jMin]];
+    yield { array, start, min, state: "post-compare" };
+
+    if (min !== start) {
+      yield { array, start, min, state: "pre-swap" };
+      [array[min], array[start]] = [array[start], array[min]];
+      yield { array, start, min, state: "post-swap" };
     }
   }
-  return temp;
+  return array;
 }
 
-function bubbleSort(array) {
-  const temp = [...array];
-  for (let i = 0; i < temp.length - 1; i++) {
-    let swapped = false;
-    for (let j = 0; j < temp.length - i - 1; j++) {
-      if (temp[j] > temp[j + 1]) {
-        [temp[j], temp[j + 1]] = [temp[j + 1], temp[j]];
-        swapped = true;
+export function* bubbleSortGenerator(initialArray) {
+  const array = [...initialArray];
+  for (let i = 0; i < array.length - 1; i++) {
+    let swapOccurred = false;
+    for (let j = 0; j < array.length - i - 1; j++) {
+      yield { array: [...array], i, j, state: "pre-compare" };
+      if (array[j] > array[j + 1]) {
+        yield { array: [...array], i, j, state: "pre-swap" };
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+        swapOccurred = true;
+        yield { array: [...array], i, j, state: "post-swap" };
       }
     }
-    if (!swapped) {
-      return temp;
+    if (!swapOccurred) {
+      break;
     }
   }
-
-  return temp;
+  return array;
 }
 
-function insertionSort(array) {
-  const temp = [...array];
+export function* insertionSortGenerator(initialArray) {
+  const array = [...initialArray];
 
-  for (let i = 1; i < temp.length; i++) {
-    const current = temp[i];
-
-    let j = i - 1;
-
-    while (j > -1 && current < temp[j]) {
-      temp[j + 1] = temp[j];
-      j--;
+  for (let i = 1; i < array.length; i++) {
+    for (let j = i - 1; j > -1 && array[j + 1] < array[j]; j--) {
+      yield { array, i, j, state: "pre-swap" };
+      [array[j + 1], array[j]] = [array[j], array[j + 1]];
+      yield { array, i, j, state: "post-swap" };
     }
-
-    temp[j + 1] = current;
   }
 
-  return temp;
+  return array;
+}
+
+export function* mergeSortGenerator(initialArray) {
+  const array = [...initialArray];
+
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const stack = [{ start: 0, end: array.length - 1 }];
+  const tempArray = Array(array.length);
+  const mergeOperations = [];
+
+  while (stack.length) {
+    const { start, end } = stack.pop();
+
+    if (start < end) {
+      const mid = Math.floor((start + end) / 2);
+      stack.push({ start: start, end: mid });
+      stack.push({ start: mid + 1, end: end });
+      mergeOperations.push({ start, mid, end });
+    }
+  }
+
+  while (mergeOperations.length) {
+    const { start, mid, end } = mergeOperations.pop();
+    let i = start;
+    let j = mid + 1;
+    let k = start;
+
+    while (i <= mid && j <= end) {
+      yield {...outputIntermediateMergeSortArray(array, tempArray, start, mid, end, i, j, k), state: "pre-compare"};
+
+      if (array[i] <= array[j]) {
+        yield {...outputIntermediateMergeSortArray(array, tempArray, start, mid, end, i, j, k), state: "pre-move-left"};
+        tempArray[k++] = array[i++];
+        yield {...outputIntermediateMergeSortArray(array, tempArray, start, mid, end, i, j, k), state: "post-move-left"};
+      } else {
+        yield {...outputIntermediateMergeSortArray(array, tempArray, start, mid, end, i, j, k), state: "pre-move-right"};
+        tempArray[k++] = array[j++];
+        yield {...outputIntermediateMergeSortArray(array, tempArray, start, mid, end, i, j, k), state: "post-move-right"};
+      }
+    }
+
+    while (i <= mid) {
+      tempArray[k++] = array[i++];
+    }
+
+    while (j <= end) {
+      tempArray[k++] = array[j++];
+    }
+
+    for (i = start; i <= end; i++) {
+      array[i] = tempArray[i];
+    }
+  }
+
+  return array;
+}
+
+function outputIntermediateMergeSortArray(array, tempArray, start, mid, end, i, j, k) {
+  const output = [...array]
+
+  let a = start;
+
+  for (let b = start; b < k; b++) {
+    output[a] = tempArray[b]
+    a++;
+  }
+  const left = a
+  for (let b = i; b <= mid; b++) {
+    output[a] = array[b];
+    a++;
+  }
+  const right = a
+  for (let b = j; b <= end; b++) {
+    output[a] = array[b]
+    a++;
+  }
+
+  return {array: output, left, right, k}
 }
